f5f9c99f702f0da06e54052d58c26e3bdbe931b4
Jovan L <327527689@qq.com>
2023-04-01 21:56:17 +0800

    added the FileMismatchMethod

---
+++ jdknewfeature/jdk12newfeature/FileMismatchMethod.java
@@ -0,0 +1,21 @@
+package jdknewfeature.jdk12newfeature;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class FileMismatchMethod {
+
+    @Test void test() throws IOException {
+        Path filePath1 = Files.createTempFile("file1", ".txt");
+        Path filePath2 = Files.createTempFile("file2", ".txt");
+        Files.writeString(filePath1, "Java 12 Article");
+        Files.writeString(filePath2, "Java 12 Article");
+
+        long mismatch = Files.mismatch(filePath1, filePath2);
+        Assertions.assertEquals(-1, mismatch);
+    }
+}
